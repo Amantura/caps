728ec9f72483e24ee0d87985a2437c5917f83fbf
amantura <amanturdev@gmail.com>
2021-04-03 13:52:28 +0600

    responcive

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="w-9/12 overflow-hidden border-2 border-gray-700 rounded-3xl">
+  <div class="overflow-hidden border-2 border-gray-700 xl:w-9/12 rounded-3xl">
     <div class="w-full py-3 text-center text-white shadow-sm bg-top-50">
       <h2>
         Калькулятор
       </h2>
     </div>
 
-    <div class="px-8 pt-4">
+    <div class="px-4 pt-4 lg:px-8">
       <p class="text-lg text-gray-400">Рассчитайте стоимость доставки <br /> вашего груза из Китая</p>
     </div>
-    <div class="flex flex-col px-8 py-4 space-y-4">
+    <div class="flex flex-col px-4 py-4 space-y-4 lg:px-8">
       <label class="relative w-full">
         <select
           id="from"
+          v-model="model.from"
           class="w-full px-5 py-5 bg-gray-100 outline-none appearance-none rounded-2xl focus:outline-none"
           name="from">
           <option value="1">Бишкек</option>
@@ -23,6 +24,7 @@
       <label class="relative">
         <select
           id="to"
+          v-model="model.to"
           class="w-full px-5 py-5 bg-gray-200 outline-none appearance-none rounded-2xl focus:outline-none"
           name="to">
           <option value="1">Бишкек</option>
@@ -31,10 +33,18 @@
       </label>
 
       <div class="flex p-1 bg-gray-200 rounded-2xl">
-        <button type="button" class="w-1/2 py-5 text-blue-500 bg-white shadow-md rounded-2xl ">
+        <button
+          type="button"
+          :class="{'text-blue-500 bg-white shadow-md': model.car === 1}"
+          class="w-1/2 py-5 rounded-2xl "
+          @click="model.car = 1">
           Фрахт
         </button>
-        <button type="button" class="w-1/2 py-5 rounded-2xl ">
+        <button
+          type="button"
+          :class="{'text-blue-500 bg-white shadow-md': model.car === 2}"
+          class="w-1/2 py-5 rounded-2xl "
+          @click="model.car = 2">
           Карго
         </button>
       </div>
@@ -42,12 +52,14 @@
       <div class="flex space-x-4">
         <label class="w-1/2">
           <input
+            v-model="model.wieght"
             type="number"
             placeholder="*Вес (кг)"
             class="w-full px-5 py-5 placeholder-gray-800 bg-gray-200 rounded-2xl " />
         </label>
         <label class="w-1/2">
           <input
+            v-model="model.size"
             type="number"
             placeholder="Объем (м3)"
             class="w-full px-5 py-5 placeholder-gray-800 bg-gray-200 rounded-2xl " />
@@ -62,18 +74,18 @@
 
       <div class="w-full bg-gray-400 h-1px"></div>
 
-      <div class="flex w-full pt-5">
-        <div class="flex flex-col w-4/12 pl-4">
+      <div class="flex w-full pt-5 space-x-3">
+        <div class="flex flex-col items-center w-4/12 lg:items-start lg:pl-4">
           <span class="text-gray-500">Цена</span>
           <span class="text-xl font-semibold text-white ">0 $/кг</span>
         </div>
 
-        <div class="flex flex-col w-4/12 pl-4">
+        <div class="flex flex-col items-center w-4/12 lg:items-start lg:pl-4">
           <span class="text-gray-500">Продажникам</span>
           <span class="text-xl font-semibold text-white ">0 $/кг</span>
         </div>
 
-        <div class="flex flex-col w-4/12 pl-4">
+        <div class="flex flex-col items-center w-4/12 lg:items-start lg:pl-4">
           <span class="text-gray-500">Агентам</span>
           <span class="text-xl font-semibold text-white ">0 $/кг</span>
         </div>
@@ -82,3 +94,17 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    model: {
+      from: '',
+      to: '',
+      car: 1,
+      weight: '',
+      size: '',
+    }
+  })
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="flex flex-col justify-center w-full min-h-screen bg-gradient-to-b from-top-50 to-gray-900">
-    <div class="container flex items-center h-full px-10 mx-auto items">
-      <div class="w-1/2 space-y-8">
-        <img src="/img/logo.svg" alt="logo caps" />
-        <h1 class="text-6xl text-white uppercase">Международные <br /> грузоперевозки <br /> из Китая</h1>
+  <div class="flex flex-col justify-center w-full min-h-screen py-10 bg-gradient-to-b from-top-50 to-gray-900">
+    <div class="container flex flex-col items-center h-full mx-auto lg:flex-row lg:px-10 items">
+      <div class="flex flex-col items-center justify-center py-10 space-y-8 lg:items-start lg:w-1/2">
+        <img
+          src="/img/logo.svg"
+          class="w-56 xl:w-auto lg:w-80"
+          alt="logo caps" />
+        <h1 class="text-xl text-center text-white uppercase lg:text-left lg:text-4xl xl:text-6xl">Международные <br /> грузоперевозки <br /> из Китая</h1>
       </div>
 
-      <div class="flex justify-center w-1/2">
+      <div class="flex justify-center lg:w-1/2">
         <Calculator />
       </div>
     </div>
