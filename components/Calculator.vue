6638aa559bf10be00c19f601c57b9a429c262467
Andrey Fedotov <protected.for@gmail.com>
2021-04-09 19:31:59 +0600

    added slice to 1 element for clearances

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent="calculate" class="overflow-hidden border-2 border-gray-700 xl:w-9/12 rounded-3xl">
+  <form class="overflow-hidden border-2 border-gray-700 xl:w-9/12 rounded-3xl" @submit.prevent="calculate">
     <div class="w-full py-3 text-center text-white shadow-sm bg-top-50">
       <h2>
         Калькулятор
@@ -20,7 +20,7 @@
             v-for="(item, fromId) in fromCities"
             :key="fromId"
             :value="item.id"
-            >{{ item.title }}</option>
+          >{{ item.title }}</option>
         </select>
         <i class="absolute text-xl text-gray-700 top-5 fas fa-caret-down right-5"></i>
       </label>
@@ -35,14 +35,14 @@
             v-for="(item, toId) in toCities"
             :key="toId"
             :value="item.id"
-            >{{ item.title }}</option>
+          >{{ item.title }}</option>
         </select>
         <i class="absolute text-xl text-gray-700 top-5 fas fa-caret-down right-5"></i>
       </label>
 
       <div class="flex p-1 bg-gray-200 rounded-2xl">
         <button
-          v-for="(item, clearanceId) in clearances"
+          v-for="(item, clearanceId) in clearances.slice(0, 1)"
           :key="clearanceId"
           type="button"
           :class="{'text-blue-500 bg-white shadow-md': model.clearance === item.id}"
@@ -72,7 +72,7 @@
       </div>
 
       <div class="pb-5">
-        <button @click="calculate" class="w-full px-5 py-4 text-white border-2 border-blue-500 bg-gradient-to-r from-blue-500 to-blue-700 rounded-3xl">
+        <button class="w-full px-5 py-4 text-white border-2 border-blue-500 bg-gradient-to-r from-blue-500 to-blue-700 rounded-3xl" @click="calculate">
           Рассчитать
         </button>
       </div>
@@ -90,10 +90,10 @@
           <span class="text-xl font-semibold text-white ">{{ model.total }}</span>
         </div>
 
-<!--        <div class="flex flex-col items-center w-4/12 lg:items-start lg:pl-4">-->
-<!--          <span class="text-gray-500">Продажникам</span>-->
-<!--          <span class="text-xl font-semibold text-white ">{{ model.sellersAmount }}</span>-->
-<!--        </div>-->
+        <!--        <div class="flex flex-col items-center w-4/12 lg:items-start lg:pl-4">-->
+        <!--          <span class="text-gray-500">Продажникам</span>-->
+        <!--          <span class="text-xl font-semibold text-white ">{{ model.sellersAmount }}</span>-->
+        <!--        </div>-->
 
         <div class="flex flex-col items-center w-4/12 lg:items-start lg:pl-4">
           <span class="text-gray-500">Агентам</span>
@@ -122,6 +122,19 @@ export default {
     fromCities: [],
     clearances: [],
   }),
+  async mounted() {
+    const fromCities = await this.$axios.$get('/api/from_cities')
+    this.model.cityFromId = fromCities[0].id
+    this.fromCities = fromCities
+
+    const toCities = await this.$axios.$get('/api/to_cities')
+    this.model.cityToId = toCities[0].id
+    this.toCities = toCities
+
+    const clearances = await this.$axios.$get('/api/clearances')
+    this.model.clearance = clearances[0].id
+    this.clearances = clearances
+  },
   methods: {
     async calculate() {
       await this.$axios.$post('/api/calculate', {
@@ -137,19 +150,6 @@ export default {
         this.model.total = data.total
       })
     }
-  },
-  async mounted() {
-    const fromCities = await this.$axios.$get('/api/from_cities')
-    this.model.cityFromId = fromCities[0].id
-    this.fromCities = fromCities
-
-    const toCities = await this.$axios.$get('/api/to_cities')
-    this.model.cityToId = toCities[0].id
-    this.toCities = toCities
-
-    const clearances = await this.$axios.$get('/api/clearances')
-    this.model.clearance = clearances[0].id
-    this.clearances = clearances
   }
 }
 </script>
